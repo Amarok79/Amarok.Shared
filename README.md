087a5924f57110f2b3dd84f2aee244459e182e08
Olaf Kober <olaf.kober@outlook.com>
2019-05-21 23:01:36 +0200

    added solution

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Amarok.Shared
+[![Build Status](https://dev.azure.com/amarok79/Amarok/_apis/build/status/Amarok.Shared)](https://dev.azure.com/amarok79/Amarok/_build/latest?definitionId=9)
+[![Build Status](https://sonarcloud.io/api/project_badges/measure?project=Amarok79_Shared&metric=alert_status)](https://sonarcloud.io/dashboard?id=Amarok79_Shared)
+[![Build Status](https://sonarcloud.io/api/project_badges/measure?project=Amarok79_Shared&metric=coverage)](https://sonarcloud.io/api/project_badges/measure?project=Amarok79_Shared&metric=coverage)
+[![NuGet](https://img.shields.io/nuget/v/Amarok.Shared.svg?logo=)](https://www.nuget.org/packages/Amarok.Shared/)
+
+## Introduction
+
+Work in Progress
---
+++ src/Amarok.Shared/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Shared
+ */
+
+using System;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+// Assembly Configuration
+[assembly: CLSCompliant(true)]
+
+// COM Configuration
+[assembly: ComVisible(false)]
+[assembly: Guid("E20AE02F-C5DB-4928-B622-4AF5A0B83145")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Friend Test Assemblies
+[assembly: InternalsVisibleTo("Tests.Amarok.Shared, PublicKey=0024000004800000940000000602000000240000525341310004000001000100691420b75c6bd08d4cde644c991ae9f5d0739ac5dc6dd6a7428fbfe03c292dae8cc9eece727e62fbbe59ef392c61ecafe563b96484e28def27eae70aff042cecaa2cfff56fed30fe6fbed507b8f159f4a43608f284a9186b8aeffd25ce944dcae0c14f61e00fe997cdf28dfa0824a65ab491aa7caddd233e6faceecadfc80eb3")]
