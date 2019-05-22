622cfb506492f4787cafe628d560b974ce549fdd
Olaf Kober <olaf.kober@outlook.com>
2019-05-22 22:51:13 +0200

    added ToLowerHex() and ToUpperHex() as extension methods

--- src/Amarok.Shared/Shared/ByteExtensions.cs
+++ src/Amarok.Shared/Shared/ByteExtensions.cs
@@ -14,7 +14,7 @@ namespace Amarok.Shared
 	public static class ByteExtensions
 	{
 		/// <summary>
-		/// Returns the hexadecimal representation of the given value.
+		/// Returns the hexadecimal representation (upper-case) of the given value.
 		/// </summary>
 		/// 
 		/// <param name="value">
@@ -23,6 +23,35 @@ namespace Amarok.Shared
 		/// <returns>
 		/// A string representation of the given value, for example, "A7" or "F0".</returns>
 		public static String ToHex(this Byte value)
+		{
+			return HexFormatter.ToUpper(value);
+		}
+
+
+		/// <summary>
+		/// Returns the lower-case hexadecimal representation of the given value.
+		/// </summary>
+		/// 
+		/// <param name="value">
+		/// The byte value to format.</param>
+		/// 
+		/// <returns>
+		/// A string representation of the given value, for example, "a7" or "f0".</returns>
+		public static String ToLowerHex(this Byte value)
+		{
+			return HexFormatter.ToLower(value);
+		}
+
+		/// <summary>
+		/// Returns the upper-case hexadecimal representation of the given value.
+		/// </summary>
+		/// 
+		/// <param name="value">
+		/// The byte value to format.</param>
+		/// 
+		/// <returns>
+		/// A string representation of the given value, for example, "A7" or "F0".</returns>
+		public static String ToUpperHex(this Byte value)
 		{
 			return HexFormatter.ToUpper(value);
 		}
--- src/Tests.Amarok.Shared/Shared/Test_ByteExtensions.cs
+++ src/Tests.Amarok.Shared/Shared/Test_ByteExtensions.cs
@@ -25,5 +25,31 @@ namespace Amarok.Shared
 			Check.That(((Byte)0xFF).ToHex())
 				.IsEqualTo("FF");
 		}
+
+		[Test]
+		public void Test_ToLowerHex()
+		{
+			Check.That(((Byte)0x00).ToLowerHex())
+				.IsEqualTo("00");
+			Check.That(((Byte)0x0F).ToLowerHex())
+				.IsEqualTo("0f");
+			Check.That(((Byte)0xF0).ToLowerHex())
+				.IsEqualTo("f0");
+			Check.That(((Byte)0xFF).ToLowerHex())
+				.IsEqualTo("ff");
+		}
+
+		[Test]
+		public void Test_ToUpperHex()
+		{
+			Check.That(((Byte)0x00).ToUpperHex())
+				.IsEqualTo("00");
+			Check.That(((Byte)0x0F).ToUpperHex())
+				.IsEqualTo("0F");
+			Check.That(((Byte)0xF0).ToUpperHex())
+				.IsEqualTo("F0");
+			Check.That(((Byte)0xFF).ToUpperHex())
+				.IsEqualTo("FF");
+		}
 	}
 }
