9e792e8b9646be4fde161543427aeb71e4fdee88
Olaf Kober <olaf.kober@outlook.com>
2019-05-22 22:17:39 +0200

    added StringBuilderPool

---
+++ src/Amarok.Shared/Shared/StringBuilderPool.cs
@@ -0,0 +1,58 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Shared
+ */
+
+using System;
+using System.Text;
+
+
+namespace Amarok.Shared
+{
+	/// <summary>
+	/// This static type provides a pool of <see cref="StringBuilder"/> instances.
+	/// </summary>
+	public static class StringBuilderPool
+	{
+		// constants
+		internal const Int32 __initialCapacity = 512;
+		internal const Int32 __maxCapacity = 40 * 1024;
+		internal const Int32 __maxNumberOfItems = 32;
+
+		// static data
+		private static readonly ObjectPool<StringBuilder> sPool = new ObjectPool<StringBuilder>(
+			() => new StringBuilder(__initialCapacity),
+			__maxNumberOfItems
+		);
+
+
+		/// <summary>
+		/// Returns a <see cref="StringBuilder"/> either from the pool or a newly created instance.
+		/// </summary>
+		public static StringBuilder Allocate()
+		{
+			return sPool.Allocate();
+		}
+
+		/// <summary>
+		/// Returns the specified <see cref="StringBuilder"/> to the pool.
+		/// 
+		/// The <see cref="StringBuilder"/> instance is cleared as part of this.
+		/// </summary>
+		/// 
+		/// <param name="builder">
+		/// The instance to return. Null values are accepted and ignored.</param>
+		public static void Free(StringBuilder builder)
+		{
+			if (builder == null)
+				return;
+
+			builder.Clear();
+
+			if (builder.Capacity > __maxCapacity)
+				builder.Capacity = __maxCapacity;
+
+			sPool.Free(builder);
+		}
+	}
+}
---
+++ src/Tests.Amarok.Shared/Shared/Test_StringBuilderPool.cs
@@ -0,0 +1,124 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Shared
+ */
+
+using System;
+using NCrunch.Framework;
+using NFluent;
+using NUnit.Framework;
+
+
+namespace Amarok.Shared
+{
+	[TestFixture, Serial, Isolated]
+	public class Test_StringBuilderPool
+	{
+		[Test]
+		public void Allocate_Free_SingleItem()
+		{
+			var sb = StringBuilderPool.Allocate();
+
+			Check.That(sb)
+				.IsNotNull();
+			Check.That(sb.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb.Length)
+				.IsEqualTo(0);
+
+			sb.Append("abc");
+
+			Check.That(sb.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb.Length)
+				.IsEqualTo(3);
+
+			StringBuilderPool.Free(sb);
+
+			Check.That(sb.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb.Length)
+				.IsEqualTo(0);
+		}
+
+		[Test]
+		public void Allocate_Free_SingleItem_MultipleTimes()
+		{
+			var sb1 = StringBuilderPool.Allocate();
+
+			Check.That(sb1.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb1.Length)
+				.IsEqualTo(0);
+
+			sb1.Append("abc");
+
+			Check.That(sb1.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb1.Length)
+				.IsEqualTo(3);
+
+			StringBuilderPool.Free(sb1);
+
+			Check.That(sb1.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb1.Length)
+				.IsEqualTo(0);
+
+			var sb2 = StringBuilderPool.Allocate();
+
+			Check.That(sb2.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb2.Length)
+				.IsEqualTo(0);
+
+			Check.That(sb2)
+				.IsSameReferenceAs(sb1);
+		}
+
+		[Test]
+		public void Free_Ignores_Null()
+		{
+			Check.ThatCode(() => StringBuilderPool.Free(null))
+				.DoesNotThrow();
+		}
+
+		[Test]
+		public void Free_Clears_StringBuilder()
+		{
+			var sb = StringBuilderPool.Allocate();
+			sb.Append("abc");
+
+			Check.That(sb.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb.Length)
+				.IsEqualTo(3);
+
+			StringBuilderPool.Free(sb);
+
+			Check.That(sb.Capacity)
+				.IsEqualTo(StringBuilderPool.__initialCapacity);
+			Check.That(sb.Length)
+				.IsEqualTo(0);
+		}
+
+		[Test]
+		public void Free_Shrinks_StringBuilder()
+		{
+			var sb = StringBuilderPool.Allocate();
+			sb.Append(new String('A', 50 * 1024));
+
+			Check.That(sb.Capacity)
+				.IsEqualTo(50 * 1024);
+			Check.That(sb.Length)
+				.IsEqualTo(50 * 1024);
+
+			StringBuilderPool.Free(sb);
+
+			Check.That(sb.Capacity)
+				.IsEqualTo(StringBuilderPool.__maxCapacity);
+			Check.That(sb.Length)
+				.IsEqualTo(0);
+		}
+	}
+}
