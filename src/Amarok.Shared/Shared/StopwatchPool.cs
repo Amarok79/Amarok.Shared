52f3e12435a011be43ea69551a58dc4879ac7e25
Olaf Kober <olaf.kober@outlook.com>
2019-05-27 21:53:44 +0200

    added StopwatchPool

---
+++ src/Amarok.Shared/Shared/StopwatchPool.cs
@@ -0,0 +1,53 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Shared
+ */
+
+using System;
+using System.Diagnostics;
+
+
+namespace Amarok.Shared
+{
+	/// <summary>
+	/// This static type provides a pool of <see cref="Stopwatch"/> instances.
+	/// </summary>
+	public static class StopwatchPool
+	{
+		// constants
+		internal const Int32 __maxNumberOfItems = 64;
+
+		// static data
+		private static readonly ObjectPool<Stopwatch> sPool = new ObjectPool<Stopwatch>(
+			() => new Stopwatch(),
+			__maxNumberOfItems
+		);
+
+
+		/// <summary>
+		/// Returns a <see cref="Stopwatch"/> either from the pool or a newly created instance.
+		/// </summary>
+		public static Stopwatch Allocate()
+		{
+			return sPool.Allocate();
+		}
+
+		/// <summary>
+		/// Returns the specified <see cref="Stopwatch"/> to the pool. 
+		/// 
+		/// The <see cref="Stopwatch"/> instance is reset as part of this.
+		/// </summary>
+		/// 
+		/// <param name="watch">
+		/// The instance to return. Null values are accepted and ignored.</param>
+		public static void Free(Stopwatch watch)
+		{
+			if (watch == null)
+				return;
+
+			watch.Reset();
+
+			sPool.Free(watch);
+		}
+	}
+}
---
+++ src/Tests.Amarok.Shared/Shared/Test_StopwatchPool.cs
@@ -0,0 +1,86 @@
+﻿/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Shared
+ */
+
+using NCrunch.Framework;
+using NFluent;
+using NUnit.Framework;
+
+
+namespace Amarok.Shared
+{
+	[TestFixture, Serial]
+	public class Test_StopwatchPool
+	{
+		[Test]
+		public void Allocate_Free_SingleItem()
+		{
+			var sw = StopwatchPool.Allocate();
+
+			Check.That(sw)
+				.IsNotNull();
+			Check.That(sw.IsRunning)
+				.IsFalse();
+
+			sw.Start();
+
+			Check.That(sw.IsRunning)
+				.IsTrue();
+
+			StopwatchPool.Free(sw);
+
+			Check.That(sw.IsRunning)
+				.IsFalse();
+		}
+
+		[Test]
+		public void Allocate_Free_SingleItem_MultipleTimes()
+		{
+			var sw1 = StopwatchPool.Allocate();
+
+			Check.That(sw1.IsRunning)
+				.IsFalse();
+
+			sw1.Start();
+
+			Check.That(sw1.IsRunning)
+				.IsTrue();
+
+			StopwatchPool.Free(sw1);
+
+			Check.That(sw1.IsRunning)
+				.IsFalse();
+
+			var sw2 = StopwatchPool.Allocate();
+
+			Check.That(sw2.IsRunning)
+				.IsFalse();
+
+			Check.That(sw2)
+				.IsSameReferenceAs(sw1);
+		}
+
+		[Test]
+		public void Free_Ignores_Null()
+		{
+			Check.ThatCode(() => StopwatchPool.Free(null))
+				.DoesNotThrow();
+		}
+
+		[Test]
+		public void Free_Resets_Stopwatch()
+		{
+			var sw = StopwatchPool.Allocate();
+			sw.Start();
+
+			Check.That(sw.IsRunning)
+				.IsTrue();
+
+			StopwatchPool.Free(sw);
+
+			Check.That(sw.IsRunning)
+				.IsFalse();
+		}
+	}
+}
