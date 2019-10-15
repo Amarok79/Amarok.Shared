88a6a868e94ebeab112fd4f06e8e30f5721abba9
Olaf Kober <olaf.kober@outlook.com>
2019-10-15 22:03:29 +0200

    .

--- src/Amarok.Shared/Shared/ObjectPool.cs
+++ src/Amarok.Shared/Shared/ObjectPool.cs
@@ -1,4 +1,7 @@
-// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+/* Copyright(c) 2019, Olaf Kober
+ * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+ * https://github.com/Amarok79/Amarok.Shared
+ */
 
 // define TRACE_LEAKS to get additional diagnostics that can lead to the leak sources. note: it will
 // make everything about 2-3x slower
