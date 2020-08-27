f93e33a817834355fb4893dfacd1bc333117b187
Olaf Kober <olaf.kober@outlook.com>
2020-08-27 23:42:32 +0200

    .

--- src/Amarok.Shared/Shared/ByteArrayExtensions.cs
+++ src/Amarok.Shared/Shared/ByteArrayExtensions.cs
@@ -1,7 +1,26 @@
-﻿/* Copyright(c) 2019, Olaf Kober
- * Licensed under GNU Lesser General Public License v3.0 (LPGL-3.0).
+﻿/* MIT License
+ * 
+ * Copyright (c) 2020, Olaf Kober
  * https://github.com/Amarok79/Amarok.Shared
- */
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
 
 #pragma warning disable S4144 // Methods should not have identical implementations
 
@@ -17,7 +36,7 @@ namespace Amarok.Shared
 	public static class ByteArrayExtensions
 	{
 		// constants
-		private const String __defaultDelimiter = "-";
+		private const String sDefaultDelimiter = "-";
 
 
 		/// <summary>
@@ -35,45 +54,8 @@ namespace Amarok.Shared
 		public static String ToHex(this Byte[] buffer)
 		{
 			Verify.NotNull(buffer, nameof(buffer));
-			return HexFormatter.ToUpper(buffer, 0, buffer.Length, __defaultDelimiter);
+			return HexFormatter.ToUpper(buffer, 0, buffer.Length, sDefaultDelimiter);
 		}
-
-		/// <summary>
-		/// Returns the lower-case hexadecimal representation of the given byte array.
-		/// </summary>
-		/// 
-		/// <param name="buffer">
-		/// The byte array to format.</param>
-		/// 
-		/// <returns>
-		/// A string representation of the given byte array, for example, "11-ff-b7".</returns>
-		/// 
-		/// <exception cref="ArgumentNullException">
-		/// Null values are invalid.</exception>
-		public static String ToLowerHex(this Byte[] buffer)
-		{
-			Verify.NotNull(buffer, nameof(buffer));
-			return HexFormatter.ToLower(buffer, 0, buffer.Length, __defaultDelimiter);
-		}
-
-		/// <summary>
-		/// Returns the upper-case hexadecimal representation of the given byte array.
-		/// </summary>
-		/// 
-		/// <param name="buffer">
-		/// The byte array to format.</param>
-		/// 
-		/// <returns>
-		/// A string representation of the given byte array, for example, "11-FF-B7".</returns>
-		/// 
-		/// <exception cref="ArgumentNullException">
-		/// Null values are invalid.</exception>
-		public static String ToUpperHex(this Byte[] buffer)
-		{
-			Verify.NotNull(buffer, nameof(buffer));
-			return HexFormatter.ToUpper(buffer, 0, buffer.Length, __defaultDelimiter);
-		}
-
 
 		/// <summary>
 		/// Returns the hexadecimal representation (upper-case) of the given byte array.
@@ -95,49 +77,6 @@ namespace Amarok.Shared
 			Verify.NotNull(delimiter, nameof(delimiter));
 			return HexFormatter.ToUpper(buffer, 0, buffer.Length, delimiter);
 		}
-
-		/// <summary>
-		/// Returns the lower-case hexadecimal representation of the given byte array.
-		/// </summary>
-		/// 
-		/// <param name="buffer">
-		/// The byte array to format.</param>
-		/// <param name="delimiter">
-		/// The delimiter to be used.</param>
-		/// 
-		/// <returns>
-		/// A string representation of the given byte array..</returns>
-		/// 
-		/// <exception cref="ArgumentNullException">
-		/// Null values are invalid.</exception>
-		public static String ToLowerHex(this Byte[] buffer, String delimiter)
-		{
-			Verify.NotNull(buffer, nameof(buffer));
-			Verify.NotNull(delimiter, nameof(delimiter));
-			return HexFormatter.ToLower(buffer, 0, buffer.Length, delimiter);
-		}
-
-		/// <summary>
-		/// Returns the upper-case hexadecimal representation of the given byte array.
-		/// </summary>
-		/// 
-		/// <param name="buffer">
-		/// The byte array to format.</param>
-		/// <param name="delimiter">
-		/// The delimiter to be used.</param>
-		/// 
-		/// <returns>
-		/// A string representation of the given byte array.</returns>
-		/// 
-		/// <exception cref="ArgumentNullException">
-		/// Null values are invalid.</exception>
-		public static String ToUpperHex(this Byte[] buffer, String delimiter)
-		{
-			Verify.NotNull(buffer, nameof(buffer));
-			Verify.NotNull(delimiter, nameof(delimiter));
-			return HexFormatter.ToUpper(buffer, 0, buffer.Length, delimiter);
-		}
-
 
 		/// <summary>
 		/// Returns the hexadecimal representation (upper-case) of the given byte array.
@@ -162,61 +101,8 @@ namespace Amarok.Shared
 		public static String ToHex(this Byte[] buffer, Int32 offset, Int32 count)
 		{
 			Verify.ArraySegment(buffer, offset, count);
-			return HexFormatter.ToUpper(buffer, offset, count, __defaultDelimiter);
+			return HexFormatter.ToUpper(buffer, offset, count, sDefaultDelimiter);
 		}
-
-		/// <summary>
-		/// Returns the lower-case hexadecimal representation of the given byte array.
-		/// </summary>
-		/// 
-		/// <param name="buffer">
-		/// The byte array to format.</param>
-		/// <param name="offset">
-		/// The byte offset in buffer where the data begin.</param>
-		/// <param name="count">
-		/// The number of bytes.</param>
-		/// 
-		/// <returns>
-		/// A string representation of the given byte array, for example, "11-22-33".</returns>
-		/// 
-		/// <exception cref="ArgumentNullException">
-		/// Null values are invalid.</exception>
-		/// <exception cref="ArgumentOutOfRangeException">
-		/// Negative values are invalid.</exception>
-		/// <exception cref="ArgumentExceedsUpperLimitException">
-		/// Values exceeding the inclusive upper limit are invalid.</exception>
-		public static String ToLowerHex(this Byte[] buffer, Int32 offset, Int32 count)
-		{
-			Verify.ArraySegment(buffer, offset, count);
-			return HexFormatter.ToLower(buffer, offset, count, __defaultDelimiter);
-		}
-
-		/// <summary>
-		/// Returns the upper-case hexadecimal representation of the given byte array.
-		/// </summary>
-		/// 
-		/// <param name="buffer">
-		/// The byte array to format.</param>
-		/// <param name="offset">
-		/// The byte offset in buffer where the data begin.</param>
-		/// <param name="count">
-		/// The number of bytes.</param>
-		/// 
-		/// <returns>
-		/// A string representation of the given byte array, for example, "11-22-33".</returns>
-		/// 
-		/// <exception cref="ArgumentNullException">
-		/// Null values are invalid.</exception>
-		/// <exception cref="ArgumentOutOfRangeException">
-		/// Negative values are invalid.</exception>
-		/// <exception cref="ArgumentExceedsUpperLimitException">
-		/// Values exceeding the inclusive upper limit are invalid.</exception>
-		public static String ToUpperHex(this Byte[] buffer, Int32 offset, Int32 count)
-		{
-			Verify.ArraySegment(buffer, offset, count);
-			return HexFormatter.ToUpper(buffer, offset, count, __defaultDelimiter);
-		}
-
 
 		/// <summary>
 		/// Returns the hexadecimal representation (upper-case) of the given byte array.
@@ -246,6 +132,72 @@ namespace Amarok.Shared
 			return HexFormatter.ToUpper(buffer, offset, count, delimiter);
 		}
 
+
+		/// <summary>
+		/// Returns the lower-case hexadecimal representation of the given byte array.
+		/// </summary>
+		/// 
+		/// <param name="buffer">
+		/// The byte array to format.</param>
+		/// 
+		/// <returns>
+		/// A string representation of the given byte array, for example, "11-ff-b7".</returns>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		public static String ToLowerHex(this Byte[] buffer)
+		{
+			Verify.NotNull(buffer, nameof(buffer));
+			return HexFormatter.ToLower(buffer, 0, buffer.Length, sDefaultDelimiter);
+		}
+
+		/// <summary>
+		/// Returns the lower-case hexadecimal representation of the given byte array.
+		/// </summary>
+		/// 
+		/// <param name="buffer">
+		/// The byte array to format.</param>
+		/// <param name="delimiter">
+		/// The delimiter to be used.</param>
+		/// 
+		/// <returns>
+		/// A string representation of the given byte array..</returns>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		public static String ToLowerHex(this Byte[] buffer, String delimiter)
+		{
+			Verify.NotNull(buffer, nameof(buffer));
+			Verify.NotNull(delimiter, nameof(delimiter));
+			return HexFormatter.ToLower(buffer, 0, buffer.Length, delimiter);
+		}
+
+		/// <summary>
+		/// Returns the lower-case hexadecimal representation of the given byte array.
+		/// </summary>
+		/// 
+		/// <param name="buffer">
+		/// The byte array to format.</param>
+		/// <param name="offset">
+		/// The byte offset in buffer where the data begin.</param>
+		/// <param name="count">
+		/// The number of bytes.</param>
+		/// 
+		/// <returns>
+		/// A string representation of the given byte array, for example, "11-22-33".</returns>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// Negative values are invalid.</exception>
+		/// <exception cref="ArgumentExceedsUpperLimitException">
+		/// Values exceeding the inclusive upper limit are invalid.</exception>
+		public static String ToLowerHex(this Byte[] buffer, Int32 offset, Int32 count)
+		{
+			Verify.ArraySegment(buffer, offset, count);
+			return HexFormatter.ToLower(buffer, offset, count, sDefaultDelimiter);
+		}
+
 		/// <summary>
 		/// Returns the lower-case hexadecimal representation of the given byte array.
 		/// </summary>
@@ -272,6 +224,72 @@ namespace Amarok.Shared
 		{
 			Verify.ArraySegment(buffer, offset, count);
 			return HexFormatter.ToLower(buffer, offset, count, delimiter);
+		}
+
+
+		/// <summary>
+		/// Returns the upper-case hexadecimal representation of the given byte array.
+		/// </summary>
+		/// 
+		/// <param name="buffer">
+		/// The byte array to format.</param>
+		/// 
+		/// <returns>
+		/// A string representation of the given byte array, for example, "11-FF-B7".</returns>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		public static String ToUpperHex(this Byte[] buffer)
+		{
+			Verify.NotNull(buffer, nameof(buffer));
+			return HexFormatter.ToUpper(buffer, 0, buffer.Length, sDefaultDelimiter);
+		}
+
+		/// <summary>
+		/// Returns the upper-case hexadecimal representation of the given byte array.
+		/// </summary>
+		/// 
+		/// <param name="buffer">
+		/// The byte array to format.</param>
+		/// <param name="delimiter">
+		/// The delimiter to be used.</param>
+		/// 
+		/// <returns>
+		/// A string representation of the given byte array.</returns>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		public static String ToUpperHex(this Byte[] buffer, String delimiter)
+		{
+			Verify.NotNull(buffer, nameof(buffer));
+			Verify.NotNull(delimiter, nameof(delimiter));
+			return HexFormatter.ToUpper(buffer, 0, buffer.Length, delimiter);
+		}
+
+		/// <summary>
+		/// Returns the upper-case hexadecimal representation of the given byte array.
+		/// </summary>
+		/// 
+		/// <param name="buffer">
+		/// The byte array to format.</param>
+		/// <param name="offset">
+		/// The byte offset in buffer where the data begin.</param>
+		/// <param name="count">
+		/// The number of bytes.</param>
+		/// 
+		/// <returns>
+		/// A string representation of the given byte array, for example, "11-22-33".</returns>
+		/// 
+		/// <exception cref="ArgumentNullException">
+		/// Null values are invalid.</exception>
+		/// <exception cref="ArgumentOutOfRangeException">
+		/// Negative values are invalid.</exception>
+		/// <exception cref="ArgumentExceedsUpperLimitException">
+		/// Values exceeding the inclusive upper limit are invalid.</exception>
+		public static String ToUpperHex(this Byte[] buffer, Int32 offset, Int32 count)
+		{
+			Verify.ArraySegment(buffer, offset, count);
+			return HexFormatter.ToUpper(buffer, offset, count, sDefaultDelimiter);
 		}
 
 		/// <summary>
